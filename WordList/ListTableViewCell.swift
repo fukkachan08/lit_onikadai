cde04eeae7339de9f30b737aba1180844a26a53d
fukkachan08 <70010364+fukkachan08@users.noreply.github.com>
2024-05-11 15:55:34 +0900

    Add files via upload

---
+++ WordList/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  WordList
+//
+//  Created by 深野真人 on 2024/05/11.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+    
+    @IBOutlet var englishLabel: UILabel!
+    @IBOutlet var japaneseLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
---
+++ WordList/WordList/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by 深野真人 on 2024/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func back(sender: UIStoryboardSegue) {
+
+    }
+    
+    @IBAction func startButtonTapped() {
+        let saveData = UserDefaults.standard
+        if saveData.array(forKey: "WORD") != nil {
+            if saveData.array(forKey: "WORD")!.count > 0 {
+                performSegue(withIdentifier: "toQuestionView", sender: nil)
+            }
+        } else {
+            let alert = UIAlertController(
+                title: "単語",
+                message: "まずは、「単語一覧」をタップして単語登録をしてください",
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(
+                title: "OK",
+                style: .default,
+                handler: nil
+            ))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+//    @IBAction func listButtonTapped() {
+//        performSegue(withIdentifier: "toListView", sender: nil)
+//    }
+}
